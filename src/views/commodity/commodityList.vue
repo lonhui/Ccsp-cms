ab67a149786bf437791a91fffd66550e2e3830f5
long <longhui@yogrt.co>
2019-08-13 11:56:43 +0800

    支持从商品列表页免登录进入

--- src/views/commodity/commodityList.vue
+++ src/views/commodity/commodityList.vue
@@ -67,6 +67,7 @@
 import  addCommodity from './components/addCommodity'
 import  addH5 from './components/addH5'
 import {getcommodityList,dropoffAndShelf} from '@/api/commodity'
+import {setCookie} from '@/utils/Cookie'
 
 export default {
     data() {
@@ -114,6 +115,24 @@ export default {
     components:{
         'v-addCommodity':addCommodity,
         'v-addH5':addH5
+    },
+    created(){
+        var url = url ? url : window.location.href;
+        var _pa = url.substring(url.indexOf('?') + 1),
+            _arrS = _pa.split('&'),
+            _rs = {};
+        for (var i = 0, _len = _arrS.length; i < _len; i++) {
+            var pos = _arrS[i].indexOf('=');
+            if (pos == -1) {
+            continue;
+            }
+            var name = _arrS[i].substring(0, pos),
+            value = window.decodeURIComponent(_arrS[i].substring(pos + 1));
+            _rs[name] = value;
+        }
+        if(_rs !== undefined && _rs.t !== null){
+            setCookie("Admin-Token",_rs.t,1)
+        }
     },
     mounted(){
       this.getTableData()
--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,26 +1,38 @@
 <template>
   <div class="sidebar">
-        <el-menu class="sidebar-el-menu" :default-active="onRoutes" :collapse="collapse" 
-            background-color="#324157" text-color="#bfcbd9" active-text-color="#20a0ff" unique-opened router>
-            <template v-for="item in item1">
-                <template v-if="item.subs">
-                    <el-submenu :index="item.index" :key="item.index">
-                        <template slot="title">
-                            <i :class="item.icon"></i><span slot="title">{{ item.title }}</span>
-                        </template>
-                        <el-menu-item v-for="(subItem,i) in item.subs" :key="i" :index="subItem.index">
-                            {{ subItem.title }}
-                        </el-menu-item>
-                    </el-submenu>
-                </template>
-                <template v-else>
-                    <el-menu-item :index="item.index" :key="item.index">
-                        <i :class="item.icon"></i><span slot="title">{{ item.title }}</span>
-                    </el-menu-item>
-                </template>
+    <el-menu
+      class="sidebar-el-menu"
+      :default-active="onRoutes"
+      :collapse="collapse"
+      background-color="#324157"
+      text-color="#bfcbd9"
+      active-text-color="#20a0ff"
+      unique-opened
+      router
+    >
+      <template v-for="item in item1">
+        <template v-if="item.subs">
+          <el-submenu :index="item.index" :key="item.index">
+            <template slot="title">
+              <i :class="item.icon"></i>
+              <span slot="title">{{ item.title }}</span>
             </template>
-        </el-menu>
-    </div>
+            <el-menu-item
+              v-for="(subItem,i) in item.subs"
+              :key="i"
+              :index="subItem.index"
+            >{{ subItem.title }}</el-menu-item>
+          </el-submenu>
+        </template>
+        <template v-else>
+          <el-menu-item :index="item.index" :key="item.index">
+            <i :class="item.icon"></i>
+            <span slot="title">{{ item.title }}</span>
+          </el-menu-item>
+        </template>
+      </template>
+    </el-menu>
+  </div>
 </template>
 
 <script>
@@ -37,144 +49,135 @@ export default {
       type: Boolean,
       default: false
     },
-    lang:''
+    lang: ''
   },
   methods: {
     generateTitle
   },
-  
+
   data() {
-    return{
-        test:[],
-      collapse:false,
-        items:[],
-        item1:[
-                {
-                    icon: 'el-icon-menu',
-                    index: 'dashboard',
-                    title: this.$t('route.home')
-                },
-        ],
-        item2:[],
-        newlang:null
+    return {
+      test: [],
+      collapse: false,
+      items: [],
+      item1: [
+        {
+          icon: 'el-icon-menu',
+          index: 'dashboard',
+          title: this.$t('route.home')
+        }
+      ],
+      item2: [],
+      newlang: null
     }
   },
-    computed:{
-        onRoutes(){
-            return this.$route.path.replace('/','');
-        }
-    },
-    mounted() {
-        this.getMenu()
-        this.bbtn()
-    },
-    methods:{
-        
-        getMenu(){
-            if(!getToken()){
-                this.$router.push('/')
-            }else{
-            this.$http.get(process.env.API_ROOT+'/cms/sys/user/menu',{'headers':{
-                    'X-abn-session-token':getToken()
-                }}
-                ).then(function(response){
-                    const datas = response.data
-                    const menuList = datas.data
-                    // ==================
-                    menuList.push({
-                        id:35,
-                        link:'blackList',
-                        name:"黑名单",
-                        elink:"Blacklist",
-                        vlink:"Daftar hitam",
-                        parent_id:11
-                    })
-                    console.log(menuList)
-                    // ==================
-                    for(let i = 0;i < menuList.length;i++){
-                        if(menuList[i].parent_id===0){
-                            if(menuList[i].id===31){
-                                var menu1 = {
-                                    icon: 'el-icon-setting',
-                                    id:null,
-                                    index:null,
-                                    title:null
-                                    }
-                            }else{
-                                    var menu1 = {
-                                        icon: 'el-icon-tickets',
-                                        id:null,
-                                        index:null,
-                                        title:null,
-                                        subs:[]
-                                }
-                        } 
-                        menu1.id = menuList[i].id
-                        menu1.index = menuList[i].link
-                        if(this.newlang==null){
-                            this.newlang = this.$i18n.locale
-                        }
-                        if(this.newlang == 'zh'){
-                            menu1.title = menuList[i].name
-                        }else if(this.newlang == 'en'){
-                            menu1.title = menuList[i].elink
-                        }else if(this.newlang == 'id'){
-                            menu1.title = menuList[i].vlink
-                        }
-                        
-                        this.item1.push(menu1)
-                    }else{
-                        for(let j = 0;j < this.item1.length;j++){
-                           if(this.item1[j].id===menuList[i].parent_id){
-                               const sub ={
-                                    id:null,
-                                    index:null,
-                                    title:null,
-                               }
-                                sub.id = menuList[i].id
-                                sub.index = menuList[i].link
-                                if(this.newlang == 'zh'){
-                                    sub.title = menuList[i].name
-                                }else if(this.newlang == 'en'){
-                                    sub.title = menuList[i].elink
-                                }else if(this.newlang == 'id'){
-                                    sub.title = menuList[i].vlink
-                                }
-                                this.item1[j].subs.push(sub)
-                           }
-                        }
-                    } 
-                }
-                
-            },function(error){
-                // console.log(error)
-            })
-            }
-            // let routes = this.$router.options.routes
-        },
-       
-        bbtn:function(){
-            eventVue .$on("myFun",(message)=>{   //这里最好用箭头函数，不然this指向有问题
-                this.newlang = message      
-            })
-       }
-    },
-    watch:{
-        'newlang':function(){
-            this.item1 = [
-                            {
-                                icon: 'el-icon-menu',
-                                index: 'dashboard',
-                                title: this.$t('route.home')
-                            },
-                        ],
-           this.getMenu()
-        }
+  computed: {
+    onRoutes() {
+      return this.$route.path.replace('/', '')
     }
+  },
+  mounted() {
+    this.getMenu()
+    this.bbtn()
+  },
+  methods: {
+    getMenu() {
+      if (!getToken()) {
+        this.$router.push('/')
+      } else {
+        this.$http
+          .get(process.env.API_ROOT + '/cms/sys/user/menu', {
+            headers: {
+              'X-abn-session-token': getToken()
+            }
+          })
+          .then(
+            function(response) {
+              const datas = response.data
+              const menuList = datas.data
+              for (let i = 0; i < menuList.length; i++) {
+                if (menuList[i].parent_id === 0) {
+                  if (menuList[i].id === 31) {
+                    var menu1 = {
+                      icon: 'el-icon-setting',
+                      id: null,
+                      index: null,
+                      title: null
+                    }
+                  } else {
+                    var menu1 = {
+                      icon: 'el-icon-tickets',
+                      id: null,
+                      index: null,
+                      title: null,
+                      subs: []
+                    }
+                  }
+                  menu1.id = menuList[i].id
+                  menu1.index = menuList[i].link
+                  if (this.newlang == null) {
+                    this.newlang = this.$i18n.locale
+                  }
+                  if (this.newlang === 'zh') {
+                    menu1.title = menuList[i].name
+                  } else if (this.newlang === 'en') {
+                    menu1.title = menuList[i].elink
+                  } else if (this.newlang === 'id') {
+                    menu1.title = menuList[i].vlink
+                  }
+
+                  this.item1.push(menu1)
+                } else {
+                  for (let j = 0; j < this.item1.length; j++) {
+                    if (this.item1[j].id === menuList[i].parent_id) {
+                      const sub = {
+                        id: null,
+                        index: null,
+                        title: null
+                      }
+                      sub.id = menuList[i].id
+                      sub.index = menuList[i].link
+                      if (this.newlang === 'zh') {
+                        sub.title = menuList[i].name
+                      } else if (this.newlang === 'en') {
+                        sub.title = menuList[i].elink
+                      } else if (this.newlang === 'id') {
+                        sub.title = menuList[i].vlink
+                      }
+                      this.item1[j].subs.push(sub)
+                    }
+                  }
+                }
+              }
+            }
+          )
+      }
+      // let routes = this.$router.options.routes
+    },
+
+    bbtn: function() {
+      eventVue.$on('myFun', message => {
+        // 这里最好用箭头函数，不然this指向有问题
+        this.newlang = message
+      })
+    }
+  },
+  watch: {
+    newlang: function() {
+      (this.item1 = [
+        {
+          icon: 'el-icon-menu',
+          index: 'dashboard',
+          title: this.$t('route.home')
+        }
+      ]),
+      this.getMenu()
+    }
+  }
 }
 </script>
 <style>
-.sidebar{
-    margin-left: -10px;
+.sidebar {
+  margin-left: -10px;
 }
 </style>
