ea65d2bb57f6f620d6b9b365a440c0374a6a47b4
long <longhui@yogrt.co>
2019-08-13 14:12:38 +0800

    修改免登录入口

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,10 +2,22 @@
   <div class="dashboard-container">
     <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
       <el-radio-group v-model="active" style="margin-bottom: 30px;">
-        <el-radio-button label="overview" :class="{active: active === 'overview'}" @click="show('overview')">{{$t('route.overview')}}</el-radio-button>
+        <el-radio-button
+          label="overview"
+          :class="{active: active === 'overview'}"
+          @click="show('overview')"
+        >{{$t('route.overview')}}</el-radio-button>
         <!-- <el-radio-button label="dailyData" :class="{ active: active === 'dailyData' }" @click="show('dailyData')">{{$t('route.dailydata')}}</el-radio-button> -->
-        <el-radio-button label="leaderboard" :class="{ active: active === 'leaderboard' }" @click="show('leaderboard')">{{$t('route.leaderboard')}}</el-radio-button>
-        <el-radio-button label="incident" :class="{ active: active === 'incident' }" @click="show('incident')">{{$t('route.event')}}</el-radio-button>
+        <el-radio-button
+          label="leaderboard"
+          :class="{ active: active === 'leaderboard' }"
+          @click="show('leaderboard')"
+        >{{$t('route.leaderboard')}}</el-radio-button>
+        <el-radio-button
+          label="incident"
+          :class="{ active: active === 'incident' }"
+          @click="show('incident')"
+        >{{$t('route.event')}}</el-radio-button>
       </el-radio-group>
       <div class="view">
         <!-- 总览 -->
@@ -22,48 +34,72 @@
 </template>
 
 <script>
-import overview from './components/overview'
+import overview from "./components/overview";
 // import dailyData from './components/dailyData'
-import leaderboard from './components/leaderboard'
-import incident from './components/incident'
+import leaderboard from "./components/leaderboard";
+import incident from "./components/incident";
 
 export default {
-  name: 'dashboard',
+  name: "dashboard",
   components: {
-    'v-overview': overview,
+    "v-overview": overview,
     // 'v-dailyData': dailyData,
-    'v-leaderboard': leaderboard,
-    'v-incident': incident
+    "v-leaderboard": leaderboard,
+    "v-incident": incident
   },
   data() {
     return {
-      active: 'overview',
+      active: "overview",
       loadingActiveData: false,
       // TODO: 默认只查7天内的数据
       days: 7
-    }
+    };
+  },
+  created() {
+    this.freeLogin();
   },
   methods: {
     show(i) {
-      this.active = i
+      this.active = i;
+    },
+    freeLogin() {
+      var url = url ? url : window.location.href;
+      var _pa = url.substring(url.indexOf("?") + 1),
+        _arrS = _pa.split("&"),
+        _rs = {};
+      for (var i = 0, _len = _arrS.length; i < _len; i++) {
+        var pos = _arrS[i].indexOf("=");
+        if (pos == -1) {
+          continue;
+        }
+        var name = _arrS[i].substring(0, pos),
+          value = window.decodeURIComponent(_arrS[i].substring(pos + 1));
+        _rs[name] = value;
+      }
+      if (_rs !== undefined && _rs.t !== null) {
+        setCookie("Admin-Token", _rs.t, 1);
+      }
     }
   }
-}
+};
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
 .dashboard {
   &-container {
     margin: 30px;
-    el-button .overview,el-button .leaderboard,el-button .dailyData,el-button .incident {
-       color: #fff;
-       background: #2896ff;
+    el-button .overview,
+    el-button .leaderboard,
+    el-button .dailyData,
+    el-button .incident {
+      color: #fff;
+      background: #2896ff;
     }
   }
 }
-  .dashboard-container{
-    width: 80%;
-    margin-left: auto;
-    margin-right: auto;
-  }
+.dashboard-container {
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+}
 </style>
